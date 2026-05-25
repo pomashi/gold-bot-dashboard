78ef377f0cb9783be45e0af44fcd6f6ea6e4ba03
pomashi <s.mekratri@gmail.com>
2026-05-25 10:31:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GOLD_DASHBOARD_CONFIG = {
   // Paste the deployed Google Apps Script Web app URL here.
-  liveDataUrl: "https://script.google.com/macros/s/AKfycbz9T90SGrGyRGb8aKpdEatDRSVj9xsr8ev4KuRDNs97N9f5JlE6bwkNRmgyLBI-JN8/exec",
+  liveDataUrl: "https://script.google.com/macros/s/AKfycbzxANvMEXaDZBxfJ4F3YFkzs1_B3M6gJgOzGDWuRCwP-1MZlsypmfjdfqMwuWBSfQMR/exec",
 };
